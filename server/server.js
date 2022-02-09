8a543597193c2aa524699db0af39a3ea2b61e357
shoddyshaman <dhananjoy.tiwari@gmail.com>
2022-02-09 11:17:06 -0700

    added styles and main.js

--- server/server.js
+++ server/server.js
@@ -12,6 +12,6 @@ app.get('/' , (req,res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4005
 
-app.listen(port, () => console.log(`server running on 4000`))
+app.listen(port, () => console.log(`server running on ${port}`))
