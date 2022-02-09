9214176e33375ecf60edd30a03faf9b2c902411f
shoddyshaman <dhananjoy.tiwari@gmail.com>
2022-02-09 11:07:28 -0700

    added styles and main.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('public'))
 
 app.get('/' , (req,res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
