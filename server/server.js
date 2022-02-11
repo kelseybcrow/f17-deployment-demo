0b8b95fba9e7d9f33d6cc2ea7f08dbc5976d25a1
Kelsey Crow <kelseybcrow@gmail.com>
2022-02-11 09:42:26 -0700

    progress

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
-const res = require('express/lib/response')
+// const res = require('express/lib/response')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {})
-res.sendFile(path.join(__dirname, '../public/index.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
 const port = process.env.PORT || 4000
 
 app.listen(port, () => console.log(`server running on 4000`))
